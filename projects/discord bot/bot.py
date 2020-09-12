965b207632afd5eb070125a10b68f3f966a685e8
Kathelyn Autumn <h0rikita.suzun3@gmail.com>
2020-09-13 00:56:52 +0800

    discord bot

---
+++ projects/discord bot/bot.py
@@ -0,0 +1,26 @@
+import discord
+import time
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'Logged on as {client.user}!')
+    channel = client.get_channel(754172127019794513)
+    while True:
+        text = input('> ')
+        if not text[0] == '#':
+            await channel.send(text)
+        else:
+            for guild in client.guilds:
+                channel = discord.utils.get(guild.text_channels, name=text[1:])
+                if channel == None:
+                    print('Channel does not exist')
+                else: print(f"Moved to #{channel.name} in '{channel.guild}'")
+
+@client.event
+async def on_message(message):
+    print('Message from {0.author}: {0.content}'.format(message))
+
+
+client.run('NzU0MTg5NjE2NTI5MTQ1OTA3.X1xH0A.787j1r42JUsydKqVIClTKg1LLzo')
