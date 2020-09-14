66c3d2e08dbe80ca9b8215de9bd609276e8b97ac
Kathelyn Autumn <h0rikita.suzun3@gmail.com>
2020-09-15 01:24:14 +0800

    working on bot.py still, color2int project

--- projects/discord bot/bot.py
+++ projects/discord bot/bot.py
@@ -4,13 +4,14 @@ def p():
     try: n += 1
     except NameError: n = 1
     print(n)
-    
+
 import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 import json
 
+## Prefix module, to be used for storing different kinds of stuff later on
 def setPrefix(bot=None, message=None, mode='r', prefix=None):
     global prefixes
     guild_id = str(message.guild.id) # json only supports string for name
@@ -35,6 +36,7 @@ def setPrefix(bot=None, message=None, mode='r', prefix=None):
         return "$"
     else: return prefixes[guild_id]
 
+## Embed module, to be used for making embeds with one line of code
 async def actionEmbed(message, args=None, **kwargs):
     embed = discord.Embed(**kwargs)
     if 'authorName' in args: embed.set_author(name=args['authorName'])
@@ -42,11 +44,16 @@ async def actionEmbed(message, args=None, **kwargs):
     if 'imageURL' in args: embed.set_image(url=args['imageURL'])
     await message.send(embed=embed)
 
+## For action embeds when multiple people are in
 def group(*args):
     args = ', '.join(list(*args[:len(*args)]))
     args = f'{args}, and {str(*args[len(*args):])}'
     return args
 
+
+##############
+###COMMANDS###
+##############
 bot = commands.Bot(command_prefix=setPrefix)
 
 @bot.event
@@ -70,31 +77,43 @@ async def on_message(message):
         print('{0.author} from {0.channel}: {0.content}'.format(message))
         await bot.process_commands(message)
 
-@bot.command()
+@bot.command(aliases=['hi'])
 async def hello(ctx):
     await ctx.send(f'Hello qtpi!')
 
 @bot.command()
-async def say(ctx,arg='What should I say?'):
-    await ctx.send(arg)
+async def say(ctx, *args):
+    await ctx.send(' '.join(args))
 
-@bot.command()
-@commands.has_permissions(administrator=True)
+@bot.command(aliases=['set'])
 async def settings(ctx, arg=None, arg2=None):
     guild = ctx.guild
     header = {'authorName':guild.name, 'authorIcon':guild.icon_url}
+    color = 2896440
     pf = setPrefix(message=ctx, mode='') # setting mode to blank as the variable from memory is only needed
     if arg == None:
-        await actionEmbed(ctx, header,
+        await actionEmbed(ctx, header, color=color,
         description=f"`prefix` : `{pf}`"
         )
     elif arg == 'prefix':
         title = 'Settings > Prefix'
         description = f'Prefix for `{guild.id}`: `{pf}`'
-        if arg2 == None: await actionEmbed(ctx, header, title=title, description=description)
+        if arg2 == None: await actionEmbed(ctx, header, title=title, description=description, color=color)
         else:
-            prefix = setPrefix(message=ctx, mode='rw', prefix=arg2)
-            await actionEmbed(ctx, header, title=title, description=f'{description} > `{prefix}`')
+            commands.has_guild_permissions(administrator=True)
+            if len(arg2) > 2: await actionEmbed(ctx, header, title=title, description=f"`{arg2}` is too long. Please keep to a minimum of 2 ")
+            else:
+                prefix = setPrefix(message=ctx, mode='rw', prefix=arg2)
+                await actionEmbed(ctx, header, title=title, description=f'{description} > `{prefix}`', color=color)
+
+#@say.error
+#async def say_error(error, ctx):
+#    if isinstance(error, commands.errors.CommandInvokeError):
+#        await ctx.send('What should I say?')
+#@settings.error
+#async def settings_error(error, ctx):
+#    if isinstance(error, commands.MissingPermissions):
+#        await actionEmbed(ctx, description=f"{error}!", color=16711680)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD')
